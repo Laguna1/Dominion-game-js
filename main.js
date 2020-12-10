98c2af7560e8e8bcec6c8d1fe6150b7dda02c38b
Paul Balitema Kabali <engineerbpk@gmail.com>
2020-12-10 20:19:04 +0300

    fetch scores

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
 const title = document.getElementById("title");
 title.innerText = "Dominion";
+
+const GAME_ID = "AI2DojFgOg4ytQpydSjH";
+const BASE_URL =
+  "https://us-central1-js-capstone-backend.cloudfunctions.net/api";
+
+const refreshBtn = document.getElementById("refresh");
+const scoresList = document.getElementById("scores");
+
+const getCurrentScores = () => {
+  fetch(`${BASE_URL}/games/${GAME_ID}/scores/`, {
+    mode: "cors",
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      scoresList.innerText = "";
+      response.result.forEach((obj) => {
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `<span>${obj.user}:</span> ${obj.score}`;
+        scoresList.appendChild(listItem);
+      });
+    });
+};
+
+refreshBtn.onclick = getCurrentScores;
